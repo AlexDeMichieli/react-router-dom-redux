3b5708dd09c96a70ff1c952d2ebd76b316cc75ef
Stanley Lim <stannielim@fb.com>
2023-09-18 21:33:16 -0400

    redux and material ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './routes/root.jsx';
-import Home from './components/Home.jsx';
-import SearchResults from './components/SearchResults.jsx';
-import Cocktail from './components/Cocktail.jsx';
-import Favs from './components/Favs.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root.jsx";
+import Home from "./components/Home.jsx";
+import SearchResults from "./components/SearchResults.jsx";
+import Cocktail from "./components/Cocktail.jsx";
+import Favs from "./components/Favs.jsx";
+
+// we use provider to let our App know about the store
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 
 const router = createBrowserRouter([
-
   {
-    path: '/',
-    element: <Root/>,
+    path: "/",
+    element: <Root />,
     children: [
       {
         index: true,
         element: <Home />,
       },
       {
-        path: '/search/:i',
+        path: "/search/:i",
         element: <SearchResults />,
       },
       {
-        path: '/cocktail/:i',
+        path: "/cocktail/:i",
         element: <Cocktail />,
       },
       {
-        path: '/favs',
+        path: "/favs",
         element: <Favs />,
       },
-
-    ]
-  }
+    ],
+  },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
